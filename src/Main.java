6b4e83a044e768aed4ae70cae08cbe92bf349e38
alijan1978 <mohammadalinaim32@gmail.com>
2023-10-24 08:11:26 -0700

    Some changes in my GitPractice class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
                 System.out.println("This is my third day of learning Git ");
+                System.out.println("This is my third day of learning Git ");
+                System.out.println("This is my third day of learning Git ");
+                System.out.println("This is my third day of learning Git ");
     }
 }
